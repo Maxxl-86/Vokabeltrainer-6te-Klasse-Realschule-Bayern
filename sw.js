c589e44aba4831ee5e06bf98331488414b830ed4
Maxxl-86 <max_bletschacher@gmx.de>
2025-12-12 08:44:38 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
+
 // Vokabeltrainer PWA – Service Worker v1 (Network-First)
 const CACHE_NAME = 'vocab-trainer-v1';
 const APP_SHELL = [
-  './', './index.html', './import.html', './style.css', './app-inline.js', './manifest.webmanifest',
-  './icon-192.png', './icon-512.png', './vocab/vocab.json'
+  './', './vokabeltrainer.html', './style.css', './trainer.js', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './data/all_units_v2.json'
 ];
-self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); });
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.matchAll({includeUncontrolled:true}).then(clients => { clients.forEach(c => c.postMessage('SW_UPDATED')); });
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
 });
-self.addEventListener('fetch', (event) => { const req = event.request; event.respondWith(
-  fetch(req).then(res => { const resClone=res.clone(); caches.open(CACHE_NAME).then(cache => cache.put(req, resClone)); return res; })
-            .catch(() => caches.match(req)) ); });
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    fetch(req).then(res => { const clone = res.clone(); caches.open(CACHE_NAME).then(c => c.put(req, clone)); return res; })
+               .catch(() => caches.match(req))
+  );
+});
